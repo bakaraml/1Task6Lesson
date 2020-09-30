63492af040dc90d3d1b22341d40bcf751a1eb8df
bakaraml <liloek2002@gmail.com>
2020-09-30 08:21:09 +0300

    add full

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,40 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long calculateSum(long[] purchases) {
+        long sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+        }
+        return sum;
+    }
+
+
+    public long findMax(long[] purchases) {
+        long currentMax = purchases[0];
+        for (long purchase : purchases) {
+            if (currentMax < purchase) {
+                currentMax = purchase;
+            }
+        }
+        return currentMax;
+    }
+
+    public long minFind(long[] purchases) {
+        long currentMin = purchases[0];
+        for (long purchase : purchases) {
+            if (currentMin > purchase) {
+                currentMin = purchase;
+            }
+        }
+        return currentMin;
+    }
+    public long middleValue(long[] purchases) {
+        long sum = 0;
+        for (long purchase : purchases) {
+            sum += purchase;
+            }
+        long result = sum / purchases.length;
+        return result;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,49 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void calculateSum() {
+        StatsService service = new StatsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.calculateSum(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax() {
+        StatsService service = new StatsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 20;
+        long actual = service.findMax(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void minFind() {
+        StatsService service = new StatsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+        long actual = service.minFind(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void middleValue() {
+        StatsService service = new StatsService();
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.middleValue(purchases);
+
+        assertEquals(expected, actual);
+    }
+}
